5f6452c568480ea6fe71b821c90b25dc572b2776
Amit Maiyar <amit.kh022@example.com>
2021-07-31 16:54:15 +0530

    Checkpoint 2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './common/Header/Header';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+// import {logo} from "./logo.svg";
 
 ReactDOM.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>,
+  <div>
+    <Header />
+  </div>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
